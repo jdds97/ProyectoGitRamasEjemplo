6c82c6808c1df83262a131142a0b682e0acf415b
Wally <jesusdediossanchez@gmail.com>
2022-11-26 18:38:50 +0100

    Holi

--- ProyectoGitRamasEjemplo/src/ProyectoGitRamasEjemplo.java
+++ ProyectoGitRamasEjemplo/src/ProyectoGitRamasEjemplo.java
@@ -3,8 +3,8 @@ import javax.swing.JOptionPane;
 public class ProyectoGitRamasEjemplo {
 
 	public static void main(String[] args) {
-		int a;
-		JOptionPane.showMessageDialog(null, "Hola Mundo");
+		
+		JOptionPane.showMessageDialog(null, "Hello Mundo");
 	
 	}
 
