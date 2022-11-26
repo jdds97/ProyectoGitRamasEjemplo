ddbc4fdd850c3f4bb5c090686133c338fbd1cec8
jdds97 <jesusdediossanchez@gmail.com>
2022-11-26 18:36:48 +0100

    Introducción método imprimeSaludo
    Introducimos el método de IimprimeSaludo directamente en github

--- ProyectoGitRamasEjemplo/src/ProyectoGitRamasEjemplo.java
+++ ProyectoGitRamasEjemplo/src/ProyectoGitRamasEjemplo.java
@@ -2,8 +2,11 @@
 public class ProyectoGitRamasEjemplo {
 
 	public static void main(String[] args) {
-		System.out.println("Hola mundo");
+	System.out.println("Hola mundo");
 
+	}
+	public static void imprimeSaludo() { 
+	System.out.println(“Hei”);	
 	}
 
 }
